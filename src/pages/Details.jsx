915648b335604efaf712ba8dc3dcc77bc8416b91
Katerina Saveleva <ash2789@mail.ru>
2022-10-29 06:02:51 -0600

    added sorting and filtering

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { IoArrowBack } from 'react-icons/io5';
+import { useHistory, useParams } from 'react-router-dom';
+import { searchByCountry } from '../config';
+import { Button } from '../components/Button';
+import { Info } from '../components/Info';
 
-export const Details = ({match}) => {
-	return (
-		<div>Details {match.params.name} </div>
-	)
-}
+export const Details = () => {
+  const { name } = useParams();
+  const { push, goBack } = useHistory();
+  const [country, setCountry] = useState(null);
+
+  console.log(country);
+
+  useEffect(() => {
+    axios.get(searchByCountry(name)).then(({ data }) => setCountry(data[0]));
+  }, [name]);
+
+  return (
+    <div>
+      <Button onClick={goBack}>
+        <IoArrowBack />
+        Back
+      </Button>
+      {country && <Info push={push} {...country} />}
+    </div>
+  );
+};
